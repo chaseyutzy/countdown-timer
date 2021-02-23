4849653568ae0cfb6ebe4cf4c73e4eca6addc7d7
Chase Yutzy <chaseyutzy@gmail.com>
2021-02-23 13:23:09 -0700

    Completed design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Countdown from "./components/Countdown";
+import FBIcon from "./assets/images/icon-facebook.svg";
+import InstaIcon from "./assets/images/icon-instagram.svg";
+import PinterestIcon from "./assets/images/icon-pinterest.svg";
 
 const App = () => {
   return (
@@ -7,6 +10,28 @@ const App = () => {
       <div id="stars" />
       <div id="innerContent">
         <Countdown />
+      </div>
+      <div id="socialWrap">
+        <div id="socialWrapInner">
+          <img
+            src={FBIcon}
+            onClick={() => console.log("FB")}
+            className="socialIcon"
+            alt="Facebook site"
+          />
+          <img
+            src={PinterestIcon}
+            onClick={() => console.log("Pinterest")}
+            className="socialIcon"
+            alt="Pinterest site"
+          />
+          <img
+            src={InstaIcon}
+            onClick={() => console.log("Instagram")}
+            className="socialIcon"
+            alt="Instagram site"
+          />
+        </div>
       </div>
     </div>
   );
